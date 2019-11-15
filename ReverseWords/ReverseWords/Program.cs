ee2a85fc39b006798de32492c2851cfd11a7d50d
unknown <nastovska.sanja@gmail.com>
2019-11-15 20:24:47 +0100

    MidTerm

---
+++ ReverseWords/ReverseWords/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace ReverseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string reverseword = "This is an example!";
+            string result = string.Join(" ", reverseword.Split(' ').Select(x => new String(x.Reverse().ToArray())));
+
+            Console.WriteLine(result);
+        }
+    }
+
+
+}
---
+++ digitalRoot/digitalRoot/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class digitalRoot
+{
+
+    static int digRoot(int n)
+    {
+        int root = 0;
+
+ 
+        while (n > 0 || root > 9)
+        {
+            if (n == 0)
+            {
+                n = root;
+                root = 0;
+            }
+
+            root += n % 10;
+            n /= 10;
+        }
+        return root;
+    }
+
+
+    public static void Main()
+    {
+        int n = 02051999;
+        Console.Write(digRoot(n));
+    }
+}
